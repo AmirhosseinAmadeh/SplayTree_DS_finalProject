b1dff6d6153fdaa1ba42814958461ec96648e45b
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-06-22 17:16:58 +0330

    createBineryTreeNode

---
+++ BTNode.java
@@ -0,0 +1,12 @@
+package SplayTree_DS_finalProject;
+
+class BTNode<T> {
+    T data;
+    BTNode<T> rightChild;
+    BTNode<T> leftChild;
+
+    public BTNode(T data) {
+        this.data = data;
+    }
+
+}
