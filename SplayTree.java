a2ad5c434020593bd4ed998976f3e82dd369aa22
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-06-23 04:22:42 +0330

    insertCompleat

--- SplayTree.java
+++ SplayTree.java
@@ -8,25 +8,90 @@ public class SplayTree {
         this.root = new BTNode(data);
     }
 
-    public boolean search(int data){
-        if(root == null) throw new IllegalStateException("root cannot be null");
-        if(root.data == data);
-        if(root.data > data);
-        if(root.data < data);
+    public boolean search(int data) {
+        if (root == null)
+            throw new IllegalStateException("root cannot be null");
+        if (root.data == data)
+            ;
+        if (root.data > data)
+            ;
+        if (root.data < data)
+            ;
         return false;
 
-        
-        
-        
-           
-        
     }
 
-    public void insert(int data){}
+    public void insert(int data) {
+        BTNode newNode = new BTNode(data);
+        BTNode y = null;
+        BTNode x = root;
 
-    public void remove(int data){}
+        while (x != null) {
+            y = x;
+            if (data < x.data)
+                x = x.leftChild;
+            else if (data > x.data)
+                x = x.rightChild;
+            else
+                return; // The node already exists, no duplicates allowed
+        }
 
-    public void sum(int start, int end){}
+        newNode.parent = y;
 
+        if (y == null)
+            root = newNode;
+        else if (data < y.data)
+            y.leftChild = newNode;
+        else
+            y.rightChild = newNode;
+
+        splay(newNode);
+    }
+
+    private void splay(BTNode x) {
+        while (x.parent != null) {
+            BTNode parent = x.parent;
+            BTNode grandParent = parent.parent;
+
+            if (grandParent == null) {
+                if (x == parent.leftChild)
+                    rotateRight(parent);
+                else
+                    rotateLeft(parent);
+            } else {
+                if (x == parent.leftChild) {
+                    if (parent == grandParent.leftChild) {
+                        rotateRight(grandParent);
+                        rotateRight(parent);
+                    } else {
+                        rotateRight(parent);
+                        rotateLeft(grandParent);
+                    }
+                } else {
+                    if (parent == grandParent.leftChild) {
+                        rotateLeft(parent);
+                        rotateRight(grandParent);
+                    } else {
+                        rotateLeft(grandParent);
+                        rotateLeft(parent);
+                    }
+                }
+            }
+        }
+
+        root = x;
+    }
+
+    private void rotateLeft(BTNode parent) {
+    }
+
+    private void rotateRight(BTNode parent) {
+    }
+
+    public void remove(int data) {
+    }
+
+    public void sum(int start, int end) {
+    }
 
 }
